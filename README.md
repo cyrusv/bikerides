77b7a9d8a34ce8995db493b577823455a88fa860
Cyrus Vafadari <cyrus@confluent.io>
2018-11-07 10:05:50 -0800

    Use local copy

---
+++ README.md
@@ -0,0 +1,1 @@
+Scraped this so I can make it more structured for filtering and searching.
--- soup.py
+++ soup.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+
 class Ride(object):
 	def __init__(self, name, description, distance, climb, fire, single, paved, road, technical, scenic):
 		self.name = name
@@ -23,30 +24,37 @@ class Ride(object):
 def getSurface(text):
 	return int(text.split('%')[0] or 0)
 
+
 def getText(el):
 	return '' if el is None else el.text
 
-r = requests.get("https://bayarearides.com/")
-soup = bs(r.text, 'html.parser')
 
-maintable = soup.find_all("table", id="snippetlist")
-assert len(maintable) == 1
-maintable = maintable[0]
+if __name__ == '__main__':
+	if True:
+		with open('rides.html', 'r') as f:
+			html = f.read()
+	else:
+		html = requests.get("https://bayarearides.com/").text
+	soup = bs(html, 'html.parser')
 
-rides = []
-for row in maintable.find_all('table', class_='rowtable'):
-	name = getText(row.find('span', class_='sortname'))
-	description = 'todo'
-	distance = getText(row.find('span', class_='length'))
-	climb = getText(row.find('span', class_='climb'))
-	fire = getText(row.find('span', class_='type_fireroad'))
-	single = getText(row.find('span', class_='type_singletrack'))
-	paved = getText(row.find('span', class_='type_paved'))
-	road = getText(row.find('span', class_='type_road'))
-	technical = getText(row.find('div', class_='rating-container-technical').find('div'))
-	scenic = getText(row.find('div', class_='rating-container-scenic').find('div'))
-	rides.append(Ride(name, description, distance, climb, fire, single, paved, road, technical, scenic))
+	maintable = soup.find_all("table", id="snippetlist")
+	assert len(maintable) == 1
+	maintable = maintable[0]
 
-print([r for r in rides if 
-	r.technical < 6 and r.single < 10
-])
+	rides = []
+	for row in maintable.find_all('table', class_='rowtable'):
+		name = getText(row.find('span', class_='sortname'))
+		description = 'todo'
+		distance = getText(row.find('span', class_='length'))
+		climb = getText(row.find('span', class_='climb'))
+		fire = getText(row.find('span', class_='type_fireroad'))
+		single = getText(row.find('span', class_='type_singletrack'))
+		paved = getText(row.find('span', class_='type_paved'))
+		road = getText(row.find('span', class_='type_road'))
+		technical = getText(row.find('div', class_='rating-container-technical').find('div'))
+		scenic = getText(row.find('div', class_='rating-container-scenic').find('div'))
+		rides.append(Ride(name, description, distance, climb, fire, single, paved, road, technical, scenic))
+
+	print([r for r in rides if
+		r.technical < 6 and r.single < 10
+	])
